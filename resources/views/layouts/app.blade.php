bdc27efe4c119d8d9c0392b1f999fb9337fbf849
Andrey Bryan M. Buzon <buzonandrey@gmail.com>
2018-03-16 13:32:35 +0800

    Few UI modification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,5 +52,25 @@
     <script>
         CKEDITOR.replace( 'article-ckeditor' );
     </script>
+
+{{--  <script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+<script>
+    $(window).scroll(function() {
+        // 100 = The point you would like to fade the nav in.
+
+        if ($(window).scrollTop() > 100) {
+
+            $('.fixed').addClass('is-sticky');
+
+        } else {
+
+            $('.fixed').removeClass('is-sticky');
+
+        };
+    });  --}}
+</script>
 </body>
 </html>
